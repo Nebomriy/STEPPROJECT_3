318ce8aa1f9209b70def39c99dd39ecfc47fa601
Andrii Seglianik <seglianik@ukr.net>
2026-03-24 23:36:09 +0100

    Add Ansible configs and Jenkinsfile

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -135,6 +135,14 @@ resource "aws_security_group" "master_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
